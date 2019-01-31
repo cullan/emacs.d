f157e2bb905e53ca109322b2b02ff63335a70bd4
Cullan Springstead <cullan.springstead@stardog.com>
2019-01-31 17:05:33 -0600

    add vterm
    interfaces with libvterm. it seems to work well so far.

---
+++ conf/libvterm.el
@@ -0,0 +1,13 @@
+;;; libvterm.el --- libvterm
+
+;;; Commentary:
+;; Exciting prospect of a real term inside Emacs.
+
+;;; Code:
+
+(add-to-list 'load-path "~/src/emacs-libvterm")
+(require 'vterm)
+
+(provide 'libvterm)
+
+;;; libvterm.el ends here
