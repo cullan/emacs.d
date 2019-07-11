790a9bcb6e2e51f948dcf1eb7d3ea1a708233564
Cullan Springstead <cullan.springstead@stardog.com>
2019-07-11 10:58:37 -0500

    install vterm from melpa and update default directory
    Set the title in the shell to user@host:path. This way C-f will start
    in the current directory in the shell.
    
    In fish, this does the trick:
    function current_dir
      echo (string replace $HOME '~' $PWD)
    end
    
    function fish_title
      echo (whoami)"@"(uname -n)":"(current_dir)
    end
    
    In bash do something like this: PS1='\[\033]0;\u@\H:\w\007\]\$ '

--- conf/libvterm.el
+++ conf/libvterm.el
@@ -5,11 +5,52 @@
 
 ;;; Code:
 
-(add-to-list 'load-path "~/src/emacs-libvterm")
-(require 'vterm)
+(defvar vterm-user "")
+(make-variable-buffer-local 'vterm-user)
+(defvar vterm-host "")
+(make-variable-buffer-local 'vterm-host)
+(defvar vterm-pwd "")
+(make-variable-buffer-local 'vterm-pwd)
+(defvar vterm-cmd "")
+(make-variable-buffer-local 'vterm-cmd)
 
-(add-hook 'vterm-exit-functions (lambda (buf)
-                                  (when buf (kill-buffer buf))))
+(defun vterm--set-title-hook (title)
+  "Use the title in the shell prompt to set the buffer title.
+TITLE = user@host@lastcmd path  or user@host path"
+  (let* ((tokens (split-string title ":" ))
+         dir)
+    (when (equal 2 (length tokens))
+      (setq vterm-pwd (string-trim-left (nth 1 tokens)))
+      (setq tokens (split-string (nth 0 tokens) "@" ))
+      (when (>  (length tokens) 1)
+        (setq vterm-user (nth 0 tokens))
+        (setq vterm-host (nth 1 tokens))
+        (when (and (nth 2 tokens)
+                   (not (string-empty-p (or (nth 2 tokens) ""))))
+          (setq vterm-cmd (nth 2 tokens))))
+      (setq dir
+            (file-name-as-directory
+             (if (and (string= vterm-host (system-name))
+                      (string= vterm-user (user-real-login-name)))
+                 (expand-file-name vterm-pwd)
+               (concat "/-:" vterm-user "@" vterm-host ":"
+                       vterm-pwd))))
+      (when (ignore-errors (file-directory-p dir))
+        (cd-absolute dir))
+      (rename-buffer (format "term %s" vterm-pwd)
+       ;; (format "term %s %s@%s:%s"
+       ;;         vterm-cmd vterm-user vterm-host vterm-pwd)
+                     t))))
+
+(use-package vterm
+  :ensure t
+  :init
+  (setq vterm-shell "/usr/local/bin/fish")
+  (add-hook 'vterm-exit-functions
+            (lambda (buf)
+              (when buf (kill-buffer buf))))
+  (add-hook 'vterm-set-title-functions
+            'vterm--set-title-hook))
 
 (provide 'libvterm)
 
