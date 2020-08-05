c5c73798e9e047542640cfb1087b027b58b41675
Cullan Springstead <cullan.springstead@stardog.com>
2020-08-05 12:34:46 -0500

    update js config to use emacs 27 built in js-mode

--- conf/javascript-conf.el
+++ conf/javascript-conf.el
@@ -5,26 +5,6 @@
 
 ;;; Code:
 
-(use-package js2-mode
-  :ensure t
-  :mode ("\\.js\\'"
-         "\\.json\\'")
-  :interpreter "node"
-  :hook (js2-mode . (lambda ()
-                      (when (string-match "\\.json\\'" (buffer-file-name))
-                        (flycheck-mode 0))))
-  :config
-  (setq js2-mode-show-parse-errors nil
-        js2-mode-show-strict-warnings nil
-        js-indent-level 2
-        js-expr-indent-offset -4
-        js-switch-indent-offset 2))
-
-(use-package rjsx-mode
-  :ensure t
-  :mode "\\.jsx\\'"
-  :magic ("^\\(.*\n\\)*.*import React" . rjsx-mode))
-
 ;; use local eslint from node_modules before global
 ;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
 (defun my/use-eslint-from-node-modules ()
@@ -38,16 +18,18 @@
       (setq-local flycheck-javascript-eslint-executable eslint))))
 (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
-;; (use-package js
-;;   :init
-;;   ;; (add-hook 'js-mode-hook #'lsp)
-;;   (setq js-indent-level 2))
-
-;; (use-package prettier-js
-;;   :ensure t
-;;   :after js2-mode
-;;   :init
-;;   (add-hook 'js2-mode-hook 'prettier-js-mode))
+(use-package js-mode
+  :mode ("\\.js\\'"
+         "\\.json\\'")
+  :interpreter "node"
+  :hook (js-mode . (lambda ()
+                     (when (string-match "\\.json\\'" (buffer-file-name))
+                       (flycheck-mode 0))))
+  :init
+  (advice-add 'js--multi-line-declaration-indentation :around (lambda (orig-fun &rest args) nil))
+  :custom
+  (js-indent-level 2)
+  (js-switch-indent-offset 2))
 
 (provide 'javascript-conf)
 
