6cd4779d6b033fdac5c91d59d317a896375710ff
Cullan Springstead <cullan.springstead@stardog.com>
2020-11-17 12:29:05 -0600

    update custom.el with latest installed packages

--- conf/custom.el
+++ conf/custom.el
@@ -10,10 +10,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default))
  '(package-selected-packages
-   '(smart-mode-line-powerline-theme smart-mode-line prettier-js rjsx-mode helm-lsp company-lsp lsp-ui lsp-mode sudo-edit forge markdown-mode flycheck-clojure helm-cider tex auctex clojure-snippets ob-elixir winum alchemist flycheck elixir-yasnippets go-mode cider elpy web-mode company elixir-mode helm-projectile magit helm paredit exec-path-from-shell use-package)))
+   '(yasnippet-snippets yaml-mode winum web-mode monokai-theme spacemacs-theme dracula-theme material-theme solarized-theme zenburn-theme sudo-edit slime yari rubocop inf-ruby smartparens rainbow-delimiters elpy paredit ob-elixir org-roam smart-mode-line-powerline-theme smart-mode-line git-link forge magit vterm helm-projectile helm go-mode flycheck-pos-tip alchemist elixir-mode company flycheck-clojure helm-cider cider clojure-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
