44d19abe7d9c7c0cc6116f7acd8d4f6ae401b382
Cullan Springstead <cullan.springstead@stardog.com>
2020-11-17 12:28:55 -0600

    add org-roam

--- conf/org-conf.el
+++ conf/org-conf.el
@@ -7,8 +7,27 @@
 
 (use-package org
   :ensure t
-  :bind (("C-c i" . org-clock-in)
-         ("C-c o" . org-clock-out)))
+  :custom
+  (org-directory "~/Documents/org/")
+  (org-agenda-files "~/Documents/org/agenda-files")
+  :bind
+  (("C-c i" . org-clock-in)
+   ("C-c o" . org-clock-out)))
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/Documents/org/notes/")
+  :hook
+  (after-init . org-roam-mode)
+  :bind (:map org-roam-mode-map
+              ("C-c n l" . org-roam)
+              ("C-c n f" . org-roam-find-file)
+              ("C-c n j" . org-roam-jump-to-index)
+              ("C-c n b" . org-roam-switch-to-buffer)
+              ("C-c n g" . org-roam-graph)
+         :map org-mode-map
+              ("C-c n i" . org-roam-insert)))
 
 (use-package ob-elixir
   :ensure t
