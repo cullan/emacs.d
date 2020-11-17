ef2cda6446c1a477add2358eafa264627c5c6ad3
Cullan Springstead <cullan.springstead@stardog.com>
2020-11-17 12:22:47 -0600

    highlight words in diffs and format git log in magit

--- conf/magit-conf.el
+++ conf/magit-conf.el
@@ -8,8 +8,11 @@
 (use-package magit
   :ensure t
   :bind ("C-c g" . magit-status)
+  :config
+  (setq magit-diff-refine-hunk 'all)
   :custom
-  (magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256"))))
+  (magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
+  (magit-log-margin '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18)))
 
 (use-package forge
   :ensure t
